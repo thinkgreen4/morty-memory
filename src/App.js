435d424d2329a8adb2b680b5a3025150436ad65a
thinkgreen4 <thinkgreen4@hotmail.com>
2018-09-26 21:09:38 -0600

    img-guess-game

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
+import Header from "./components/Header";
 import FriendCard from "./components/FriendCard";
 import friends from "./friends.json";
 import "./app.css"
@@ -8,33 +8,56 @@ import "./app.css"
 class App extends Component {
   state = {
     friends,
-    count: 0
+    score: 0,
+    highscore: 0
   };
-  handleIncrement = () => {
-    // We always use the setState method to update a component's state
-    this.setState({ count: this.state.count + 1 });
-  };
-changeState = id => {
-  const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
 
-};
+  gameOver = () => {
+    if (this.state.score > this.state.highscore) {
+      this.setState({highscore: this.state.score}, function() {
+        console.log(this.state.highscore);
+      });
+    }
+    this.state.friends.forEach(friend => {
+      friend.count = 0;
+    });
+    alert(`Game Over :( \nscore: ${this.state.score}`);
+    this.setState({score: 0});
+    return true;
+  }
+  clickCount = id => {
+    this.state.friends.find((o, i) => {
+      if (o.id === id) {
+        if(friends[i].count === 0){
+          friends[i].count = friends[i].count + 1;
+          this.setState({score : this.state.score + 1}, function(){
+            console.log(this.state.score);
+          });
+          this.state.friends.sort(() => Math.random() - 0.5)
+          return true; 
+        } else {
+          this.gameOver();
+        }
+      }
+    });
+  }
+
 render() {
-  return (
-      <Wrapper>
-    <Title>Image Guess Game   Click each image once, click the same image and lose!         Score:{this.state.count}    Top Score:{}</Title>
+  
+return (
+  <Wrapper>
+    <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
     {this.state.friends.map(friend => (
-   <FriendCard
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-             />
-    ))};
+      <FriendCard
+        clickCount={this.clickCount}
+        id={friend.id}
+        key={friend.id}
+        image={friend.image}
+      />
+    ))}
   </Wrapper>
    );
   }
-}
 
+ }
 export default App;
--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -2,18 +2,10 @@ import React from "react";
 import "./FriendCard.css";
 
 const FriendCard = props => (
-  <div className="card" onClick={() => props.changeState(props.id)}>
+<div className="card" onClick={() => props.clickCount(props.id)}>
     <div className="img-container">
       <img alt={props.name} src={props.image} />
     </div>
-    <div className="content">
-      <ul>
-        <li>
-          <strong>Name:</strong> {props.name}
-        </li>
-      </ul>
-    </div>
-   
   </div>
 );
 
